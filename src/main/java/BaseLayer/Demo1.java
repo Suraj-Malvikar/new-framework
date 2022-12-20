cb8cd3469cb13855cd50099db184f5672ecb814e
unknown <madhuritakate918@gmail.com>
2022-12-20 23:37:02 +0530

    Demo1 created

---
+++ src/main/java/BaseLayer/Demo1.java
@@ -0,0 +1,11 @@
+package BaseLayer;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo1 calss created");
+
+	}
+
+}
